347133cb8d3efe0314837f06b76b4626a68b7bb3
Ruben Taelman <rubensworks@gmail.com>
2019-04-26 21:16:20 +0200

    Increase default bless delay and make configurable, Closes #2

--- src/main/java/org/cyclops/everlastingabilitiespotioncore/ability/config/AbilityBlessConfig.java
+++ src/main/java/org/cyclops/everlastingabilitiespotioncore/ability/config/AbilityBlessConfig.java
@@ -1,11 +1,11 @@
 package org.cyclops.everlastingabilitiespotioncore.ability.config;
 
 import com.tmtravlr.potioncore.potion.PotionBless;
-import com.tmtravlr.potioncore.potion.PotionRepair;
 import net.minecraft.item.EnumRarity;
 import org.cyclops.cyclopscore.config.ConfigurableProperty;
 import org.cyclops.cyclopscore.config.ConfigurableTypeCategory;
 import org.cyclops.cyclopscore.config.configurable.IConfigurable;
+import org.cyclops.cyclopscore.helper.MinecraftHelpers;
 import org.cyclops.everlastingabilities.ability.AbilityTypePotionEffectSelf;
 import org.cyclops.everlastingabilities.core.config.extendedconfig.AbilityConfig;
 
@@ -25,12 +25,17 @@ public class AbilityBlessConfig extends AbilityConfig {
      * The maximum ability level.
      */
     @ConfigurableProperty(category = ConfigurableTypeCategory.GENERAL, categoryRaw = "ability", comment = "The maximum ability level.", requiresMcRestart = true)
-    public static int maxLevel = 5;
+    public static int maxLevel = 6;
     /**
      * The xp required per level.
      */
     @ConfigurableProperty(category = ConfigurableTypeCategory.GENERAL, categoryRaw = "ability", comment = "The xp required per level.", requiresMcRestart = true)
     public static int xpPerLevel = 25;
+    /**
+     * The basis tick delay after which this ability should apply. This is divided by the active level.
+     */
+    @ConfigurableProperty(category = ConfigurableTypeCategory.GENERAL, categoryRaw = "ability", comment = "The basis tick delay after which this ability should apply. This is divided by the active level.", requiresMcRestart = true)
+    public static int baseTickDelay = MinecraftHelpers.SECOND_IN_TICKS * 10;
 
     /**
      * The unique instance.
@@ -50,6 +55,21 @@ public class AbilityBlessConfig extends AbilityConfig {
 
     @Override
     protected IConfigurable initSubInstance() {
-        return new AbilityTypePotionEffectSelf(getNamedId(), rarity, maxLevel, xpPerLevel, PotionBless.INSTANCE);
+        return new AbilityTypePotionEffectSelf(getNamedId(), rarity, maxLevel, xpPerLevel, PotionBless.INSTANCE){
+            @Override
+            protected int getDuration(int tickModulus, int level) {
+                return 1;
+            }
+
+            @Override
+            protected int getTickModulus(int level) {
+                return AbilityBlessConfig.baseTickDelay * getMaxLevelInfinitySafe() / level;
+            }
+
+            @Override
+            protected int getAmplifier(int level) {
+                return 0;
+            }
+        };
     }
 }
